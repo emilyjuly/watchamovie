ce4da62be8294fef29865ce33f2c0411517710e6
Emily <ejuly@indracompany.com>
2023-07-28 13:18:15 -0300

    Criando pages

--- watchamovie/src/main.jsx
+++ watchamovie/src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+import Movie from "./pages/Movie.jsx";
+import Search from "./pages/Search.jsx";
+import App from "./App.jsx";
 
 import "./index.css";
 
