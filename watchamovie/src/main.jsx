81814dba3ea9399cb3d5c190d6dd66c7ce0e96e6
Emily <ejuly@indracompany.com>
2023-07-28 13:12:40 -0300

    Implementando react-router-dom

--- watchamovie/src/main.jsx
+++ watchamovie/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route path="search" element={<Search />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
