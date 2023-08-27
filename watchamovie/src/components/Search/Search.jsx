a65354502500f3eb6868be261d45cd942f55abdd
Emily <ejuly@minsait.com>
2023-08-27 16:26:43 -0300

    Removendo animação do search

--- watchamovie/src/components/Search/Search.jsx
+++ watchamovie/src/components/Search/Search.jsx
@@ -20,22 +20,22 @@ const Search = () => {
   return (
     <div>
       <form className="search-container" onSubmit={handleSubmit}>
-        <div id="container-stars">
-          <div><p className="navbar-title">
-            Welcome!  <br /> <span className="phrase">What are you looking for to watch today?</span>
-          </p></div>
-          <div className="search-input">
-            <input
-              type="text"
-              placeholder="Search for a movie"
-              className="input"
-              onChange={(e) => setSearch(e.target.value)}
-              value={search}
-            />
-            <button type="submit" className="btn-search">
-              <BiSearchAlt className="search-icon" />
-            </button>
-          </div>
+
+        <div><p className="navbar-title">
+          Welcome!  <br /> <span className="phrase">What are you looking for to watch today?</span>
+        </p></div>
+        <div className="search-input">
+          <input
+            type="text"
+            placeholder="Search for a movie"
+            className="input"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
+          <button type="submit" className="btn-search">
+            <BiSearchAlt className="search-icon" />
+          </button>
+
         </div>
       </form>
     </div>
